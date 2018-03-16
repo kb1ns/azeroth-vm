009d8c9cf87f07ccfa0add286128a3c2a4f9cb61
zhangyu <zhangyu@zhangyudeMacBook-Pro.local>
2018-03-16 11:53:42 +0800

    jvm_stack

--- src/mem/stack.rs
+++ src/mem/stack.rs
@@ -1,1 +1,22 @@
-pub type Stack = Vec<u8>;
+pub struct Stack {
+    pub jvm_method: Vec<Frame>,
+    pub jvm_pc: usize,
+    pub max_stack_size: usize,
+}
+
+pub struct Frame {
+    locals: Vec<u8>,
+    operands: Vec<u8>,
+}
+
+impl Stack {
+
+    pub fn allocate(max_stack_size: usize, pc: usize) -> Stack {
+        Stack {
+            jvm_method: Vec::<Frame>::new(),
+            jvm_pc: pc,
+            max_stack_size: max_stack_size,
+        }
+    }
+
+}
